6f48fb80e589685401acbf373edcaa034b68d0b4
Chiuri254 <joseshiory@gmail.com>
2019-03-11 00:48:25 +0300

    Create views

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile,Posts,Like,Follow
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Posts)
+admin.site.register(Like)
+admin.site.register(Follow)
+
