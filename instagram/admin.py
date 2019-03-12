d8a76c35a59c68ab7a47bd32f032198f5e783b5b
Chiuri254 <joseshiory@gmail.com>
2019-03-12 10:31:01 +0300

    install gunicorn

--- instagram/admin.py
+++ instagram/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Profile,Posts,Like,Follow
 
 # Register your models here.
-admin.site.register(Profile)
-admin.site.register(Posts)
-admin.site.register(Like)
-admin.site.register(Follow)
+# admin.site.register(Profile)
+# admin.site.register(Posts)
+# admin.site.register(Like)
+# admin.site.register(Follow)
 
